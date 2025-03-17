cadbe22d74a6cfd9fdb0e35c3303684749df1aed
Simun 234 <your-email@example.com>
2025-03-17 18:29:25 +0100

    Put Navbar in HomePage
    Implemented a responsive navigation bar
    
    Added mobile menu toggle with open/close functionality
    
    Included a shopping cart icon in both mobile and desktop views
    
    Used React Router for navigation links

--- medical-facilities-app/src/components/HomePage.jsx
+++ medical-facilities-app/src/components/HomePage.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Navbar from './Navbar';
 
 function HomePage() {
 
 
   return (
     <>
-      <h1>Home Page</h1>
+      <Navbar />
     </>
   )
 }
--- medical-facilities-app/src/components/Navbar.jsx
+++ medical-facilities-app/src/components/Navbar.jsx
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+import { HiMenu } from "react-icons/hi";
+import { IoMdClose } from "react-icons/io";
+import { FaShoppingCart } from "react-icons/fa";
 
-function Navbar() {
+
+const Navbar = () => {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+  const closeMenu = () => setMenuOpen(false);
 
 
   return (
     <>
-      <h1>Navbar Page</h1>
+    <header className='w-[245px] h-[38px] flex flex-row items-center '></header>
+    <div className='flex flex-row gap-5 items-center'>
+    <button 
+          className="block md:hidden text-black text-3xl" 
+          onClick={toggleMenu}
+        >
+          {menuOpen ? <IoMdClose /> : <HiMenu />}
+        </button>
+        
+        {menuOpen && (
+          <div className="absolute top-16 left-0 w-full bg-white shadow-md p-4 flex flex-col items-center md:hidden">
+            <Link to="/" className="text-black hover:underline mb-2" onClick={closeMenu}>
+             Home
+            </Link>
+            <Link to="/about-page" className="text-black hover:underline mb-2" onClick={closeMenu}>
+              About Us
+            </Link>
+          </div>
+        )}
+      <p className='font-serif font-normal text-black text-sm md:hidden '>Menu</p>
+      <FaShoppingCart className='ml-60 md:hidden' />
+      </div>
+<div className="flex flex-row items-center gap-6 float-right mr-10">
+          <Link to="/" className="text-black hover:underline">
+            Home
+          </Link>
+          <Link to="/about-page" className="text-black hover:underline">
+           About Us
+          </Link>
+          <FaShoppingCart />
+          <p className='font-serif font-normal text-black'>Cart</p>
+        </div>
+
     </>
   )
 }
