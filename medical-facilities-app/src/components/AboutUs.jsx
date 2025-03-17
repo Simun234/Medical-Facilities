8ad46d46e1c0006b2030bba5e25a91c4d2e9b3ff
Simun 234 <your-email@example.com>
2025-03-17 17:31:31 +0100

    Installed everything for new react+TailwindCss project
    Created new Folder components with three new file

---
+++ medical-facilities-app/src/components/AboutUs.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function About() {
+
+
+  return (
+    <>
+      <h1>About Us Page</h1>
+    </>
+  )
+}
+
+export default About;
---
+++ medical-facilities-app/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navbar() {
+
+
+  return (
+    <>
+      <h1>Navbar Page</h1>
+    </>
+  )
+}
+
+export default Navbar;
